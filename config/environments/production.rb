9a189c2820ca3f4462d2972010402de28394329f
Nitin Agarwal <nitin9003@gmail.com>
2013-06-19 16:21:17 +0530

    added precompile for css in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Iface::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
- # config.assets.compile = true
+  config.assets.compile = true
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
